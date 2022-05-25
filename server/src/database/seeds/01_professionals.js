9968da278c41280b4460cfc0e0ce56a2533e331d
Acacio de Lima <32038004+limazia@users.noreply.github.com>
2022-05-25 15:56:28 -0300

    Update 01_professionals.js

--- server/src/database/seeds/01_professionals.js
+++ server/src/database/seeds/01_professionals.js
@@ -1,12 +1,13 @@
 exports.seed = function (knex) {
-  knex("professionals").insert([
+  return knex("professionals").insert([
     {
       id: "0361ddbbcc92022",
       name: "Acacio de Lima",
       email: "limadeacacio@gmail.com",
       password: "$2b$10$qciRG8CMZlnJHdqfc9NDcu.wuadRxuQBU9.neNPO09tD0vnUJ7Rii",
       primary_user: 1,
-    }, {
+    },
+    {
       id: "241c516770a4eda",
       name: "Leonardo Ximenes",
       email: "leo@gmail.com",
