73f7cebd8abe6fed932a38f45df9605e83e9223b
Acacio de Lima <32038004+limazia@users.noreply.github.com>
2022-05-25 15:20:43 -0300

    Update and rename professionals.js to 01_professionals.js

--- server/src/database/seeds/01_professionals.js
+++ server/src/database/seeds/01_professionals.js
@@ -6,8 +6,11 @@ exports.seed = function (knex) {
       email: "limadeacacio@gmail.com",
       password: "$2b$10$qciRG8CMZlnJHdqfc9NDcu.wuadRxuQBU9.neNPO09tD0vnUJ7Rii",
       primary_user: 1,
-      updateAt: "2022-04-23 13:16:45",
-      createdAt: "2022-04-23 13:16:45"
+    }, {
+      id: "241c516770a4eda",
+      name: "Leonardo Ximenes",
+      email: "leo@gmail.com",
+      password: "2b$10$u7kMUW7zf2W.bMhwSel2BuOLXA85Dkq5oBvDYI0fvk/GvWuWqN0Ve",
     },
   ]);
 };
