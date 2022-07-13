32ae4d8901390a3875bc92fdbbc92b610ae1c6bd
Acacio de Lima <limadeacacio@gmail.com>
2022-07-13 02:35:32 -0300

    Update database.config.js

--- server/src/config/database.config.js
+++ server/src/config/database.config.js
@@ -3,13 +3,13 @@ const { env } = require("./../helpers/utils.helper");
  
 module.exports = {
   development: {
-    client: env("DB_DRIVE"),
+    client: env("DB_DRIVE", "mysql"),
     connection: {
-      host: env("DB_HOST"),
+      host: env("DB_HOST", "localhost"),
       port: env("DB_PORT", 3306),
-      user: env("DB_USERNAME"),
-      password: env("DB_PASSWORD"),
-      database: env("DB_DATABASE"),
+      user: env("DB_USERNAME", "root"),
+      password: env("DB_PASSWORD", ""),
+      database: env("DB_DATABASE", ""),
     },
     pool: {
       min: 2,
@@ -24,13 +24,13 @@ module.exports = {
   },
 
   production: {
-    client: env("DB_DRIVE"),
+    client: env("DB_DRIVE", null),
     connection: {
-      host: env("DB_HOST"),
+      host: env("DB_HOST", null),
       port: env("DB_PORT", 3306),
-      user: env("DB_USERNAME"),
-      password: env("DB_PASSWORD"),
-      database: env("DB_DATABASE"),
+      user: env("DB_USERNAME", null),
+      password: env("DB_PASSWORD", null),
+      database: env("DB_DATABASE", null),
     },
     migrations: {
       directory: path.resolve(__dirname, "../", "database", "migrations"),
